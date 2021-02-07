bbe21f26daf8571a11801a9152582ac601ebd9d0
Eric Chen <ericchen201896@gmail.com>
2021-02-06 23:02:52 -0800

    Added in Exercise model

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const NoteSchema = new Schema({
-  title: String,
-  body: String
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: "String is required"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "String is required"
+    },
+    duration: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
 });
 
-const Note = mongoose.model("Note", NoteSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-module.exports = Note;
+module.exports = Exercise;
