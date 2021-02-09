a8c3a64724299def835788c53ea4095a74b5e313
Eric Chen <ericchen201896@gmail.com>
2021-02-08 18:04:08 -0800

    added i put route which updates the workout by adding in exercise

--- models/Workout.js
+++ models/Workout.js
@@ -10,11 +10,78 @@ const WorkoutSchema = new Schema({
     },
     exercises: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
+            type: {
+                type: String,
+                trim: true,
+                required: "String is required"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "String is required"
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
         }
     ]
 });
+// numExercises: "Exercises Performed",
+// totalWeight: "Total Weight Lifted",
+// totalSets: "Total Sets Performed",
+// totalReps: "Total Reps Performed",
+// totalDistance: "Total Distance Covered"
+WorkoutSchema.virtual("totalDuration").get(function () {
+    let result = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        result += this.exercises[i].duration;
+    }
+    return result;
+});
+
+WorkoutSchema.virtual("numExercises").get(function () {
+    return this.exercises.length;
+});
+
+WorkoutSchema.virtual("totalWeight").get(function () {
+    let result = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        result += this.exercises[i].weight;
+    }
+    return result;
+});
+WorkoutSchema.virtual("totalSets").get(function () {
+    let result = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        result += this.exercises[i].sets;
+    }
+    return result;
+});
+WorkoutSchema.virtual("totalReps").get(function () {
+    let result = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        result += this.exercises[i].reps;
+    }
+    return result;
+});
+WorkoutSchema.virtual("totalDistance").get(function () {
+    let result = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        result += this.exercises[i].distance;
+    }
+    return result;
+});
+
+WorkoutSchema.set('toJSON', { virtuals: true })
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public", { extensions: "html" }));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+//THIS WORKS
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
+        .then(dbExercise => {
+            res.json(dbExercise);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+    // db.Workout.create(body)
+    //     .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
+    //     .then(dbExercise => {
+    //         res.json(dbExercise);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
