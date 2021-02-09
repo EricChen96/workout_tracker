25558190b8a2c82580949f4a63246fec6b39486a
Eric Chen <ericchen201896@gmail.com>
2021-02-08 18:04:46 -0800

    Added in create workout route

--- server.js
+++ server.js
@@ -45,6 +45,17 @@ app.put("/api/workouts/:id", (req, res) => {
     //     });
 });
 
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
